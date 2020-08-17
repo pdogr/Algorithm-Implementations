ea2a89d5978b8be2a218c24d36746ff250e4a7f4
plaxi0s <pawan.dogra99@gmail.com>
2020-08-18 05:19:21 +0530

    Transforms

---
+++ maths/andFWHT.h
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LL long long
+
+namespace Convolution {
+
+void andFWHT(vector<LL> &A, bool iv = 0) { // matrix [0 1][1 1]
+	int n = A.size();
+	for (int s = 1; s < n; s <<= 1) {
+		for (int i = 0; i < n; i += 2 * s) {
+			for (int j = 0; j < s; ++j) {
+				cd u = A[i + j], v = A[i + j + s];
+				if (!iv) {
+					A[i + j] = v;
+					A[i + j + s] = u + v;
+				}
+				else {
+					A[i + j] = v - u;
+					A[i + j + s] = v;
+				}
+			}
+		}
+	}
+}
+
+}
---
+++ maths/xorFWHT.h
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LL long long
+
+namespace Convolution {
+
+void xorFWHT(vector<LL> &A, bool iv = 0) { // matrix [1 1][1 -1]
+	int n = A.size();
+	for (int s = 1; s < n; s <<= 1) {
+		for (int i = 0; i < n; i += 2 * s) {
+			for (int j = 0; j < s; ++j) {
+				cd u = A[i + j], v = A[i + j + s];
+				A[i + j] = u + v;
+				A[i + j + s] = u - v;
+			}
+		}
+	}
+	if (iv) {
+		for (int i = 0; i < n; ++i)A[i] /= n;
+	}
+}
+
+}
